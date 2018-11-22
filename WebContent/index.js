31d894bc28881bc7ff1fe1d26a8952e6ac476e3d
cch <1136856486@qq.com>
2018-11-22 08:27:25 +0800

    he

--- WebContent/index.js
+++ WebContent/index.js
@@ -24,3 +24,82 @@ function testAjax(){
 		console.log("not support XMLHttp");
 	}
 }
+
+
+function checkForm(formId){  
+    var params = serializeForm(formId);  
+    console.log(params); 
+    return true;
+}  
+
+
+function getElements(formId) {    
+    var form = document.getElementById(formId);    
+    var elements = new Array();  
+    console.log(elements);
+    var tagElements = form.getElementsByTagName('input');    
+    for (var j = 0; j < tagElements.length; j++){  
+         elements.push(tagElements[j]);  
+  
+    }  
+   /* var selectElements = form.getElementsByTagName('select');    
+    for (var j = 0; j < selectElements.length; j++){  
+    	elements.push(selectElements[j]);  
+    	
+    }  */
+    return elements;    
+}   
+  
+
+function inputSelector(element) {    
+  if (element.checked)    
+     return [element.name, element.value];    
+}    
+      
+function input(element) {    
+    switch (element.type.toLowerCase()) {    
+      case 'submit':    
+      case 'hidden':    
+      case 'password':    
+      case 'text':    
+        return [element.name, element.value];    
+      case 'checkbox':    
+      case 'radio':    
+        return inputSelector(element);    
+    }    
+    return false;    
+}    
+  
+function serializeElement(element) {    
+    var method = element.tagName.toLowerCase();    
+    var parameter = input(element);    
+    
+    if (parameter) {    
+      var key = encodeURIComponent(parameter[0]);    
+      if (key.length == 0) return;    
+    
+      if (parameter[1].constructor != Array)    
+        parameter[1] = [parameter[1]];    
+          
+      var values = parameter[1];    
+      var results = [];    
+      for (var i=0; i<values.length; i++) {    
+        results.push(key + '=' + encodeURIComponent(values[i]));    
+      }    
+      return results.join('&');    
+    }    
+ }    
+  
+
+function serializeForm(formId) {    
+    var elements = getElements(formId);    
+    var queryComponents = new Array();    
+    
+    for (var i = 0; i < elements.length; i++) {    
+      var queryComponent = serializeElement(elements[i]);    
+      if (queryComponent)    
+        queryComponents.push(queryComponent);    
+    }    
+    
+    return queryComponents.join('&');  
+}    
